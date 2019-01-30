c15660916b98b928d7405ee28fe61b26c1d44815
Alejandra <aleizaguirrehernandez@gmail.com>
2019-01-30 11:51:13 +0100

    Carrera terminada

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class circuito():
     corredores = []
@@ -15,16 +16,31 @@ class circuito():
         
         self.__createRunners()
         
-        def __createRunners(self):
-            for i in range(4):
-                new_turtle = turtle.Turtle()
-                new_turtle.color(self.__colorTurtle[i])
-                new_turtle.shape("turtle")
-                new_turtle.penup()
-                new_turtle.setpos(self.__starLine, self.posStartY[i])
-                
-                self.corredores.append(new_turtle)
+    def __createRunners(self):
+        for i in range(4):
+            new_turtle = turtle.Turtle()
+            new_turtle.color(self.__colorTurtle[i])
+            new_turtle.shape("turtle")
+            new_turtle.penup()
+            new_turtle.setpos(self.__starLine, self.__posStartY[i])
             
+            self.corredores.append(new_turtle)
+    
+    def competir(self):        
+        hayGanador = False
+        
+        while not hayGanador:
+            for tortuga in self.corredores:
+                avance = random.randint(1,6)
+                tortuga.forward(avance)
+                
+                if tortuga.position()[0] >= self.__finishLine:
+                    hayGanador = True
+                    print("La tortuga de color {} ha ganado".format(tortuga.color()[0]))
+                    
+                
+                        
             
 if __name__ == "__main__":
     circuito = circuito(640,480)
+    circuito.competir()
